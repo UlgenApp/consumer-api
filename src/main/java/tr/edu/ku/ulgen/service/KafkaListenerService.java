30d46a80f90fd3f789b69c8eaaa1095c0a6d23c6
Kaan Türkmen <kaanturkmen@protonmail.com>
2023-04-02 13:30:00 +0300

    Added logging.

--- src/main/java/tr/edu/ku/ulgen/service/KafkaListenerService.java
+++ src/main/java/tr/edu/ku/ulgen/service/KafkaListenerService.java
@@ -3,7 +3,7 @@ package tr.edu.ku.ulgen.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import tr.edu.ku.ulgen.dto.UlgenDto;
@@ -12,15 +12,18 @@ import tr.edu.ku.ulgen.repository.UlgenDataRepository;
 
 @Component
 @AllArgsConstructor
+@Slf4j
 public class KafkaListenerService {
     private UlgenDataRepository ulgenDataRepository;
 
     @KafkaListener(topics = "ulgen", groupId = "groupId")
     void listener(String raw) throws JsonProcessingException {
+        log.info("Raw message is received by the listener: {}", raw);
+
         ObjectMapper mapper = new ObjectMapper();
         UlgenDto ulgenDto = mapper.readValue(raw, UlgenDto.class);
 
-        System.out.println("Listener got: " + ulgenDto.toString());
+        log.info("Mapped raw message to the UlgenDto.");
 
         ulgenDataRepository
                 .saveOrUpdate(UlgenData.builder()
